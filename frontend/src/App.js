869fcb3b73824aaff6918ea69782542eeae6555b
Pedro Lucas <pedrolucas122737@gmail.com>
2023-05-16 10:14:17 -0300

    Configurando react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <div className="App">
-      <h1>shop naw</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
