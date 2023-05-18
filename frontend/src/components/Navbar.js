9883d590ea1c500f56f723af044cfc16e5451ad1
Pedro Lucas <pedrolucas122737@gmail.com>
2023-05-18 11:01:53 -0300

    Navbar para usuários autenticados

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,12 +10,15 @@ import {
 } from "react-icons/bs";
 
 // Hooks
-// import { useState } from "react";
-// import { useAuth } from "../hooks/useAuth";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav id="nav">
       <Link to="/">Shop Naw</Link>
@@ -24,17 +27,39 @@ const Navbar = () => {
         <input type="text" />
       </form>
       <ul id="nav-links">
-        <li>
-          <NavLink>
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink to="/Register">Cadastrar</NavLink>
-        </li>
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill /> {/*Ver o que está postado*/}
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to="/profile">
+                <BsFillPersonFill /> {/*Modificar dados do perfil*/}
+              </NavLink>
+            </li>
+            <li>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to="/login">Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Register">Cadastrar</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
