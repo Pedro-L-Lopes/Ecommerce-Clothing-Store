c68a78b236e0167bcc1eaf5e06a3ed2e795ee0d3
Pedro Lucas <pedrolucas122737@gmail.com>
2023-07-17 21:45:12 -0300

    Finalizando carrinho de compras

--- frontend/src/pages/Store/Cart/Cart.js
+++ frontend/src/pages/Store/Cart/Cart.js
@@ -1,27 +1,38 @@
+// Hooks
 import React, { useEffect, useState } from "react";
+
+// Redux
 import { useSelector, useDispatch } from "react-redux";
 import { removeCart } from "../../../slices/cartSlice";
 import {
   fetchShippingByCep,
   fetchCalculateTermsAndPrice,
 } from "../../../slices/shippingSlice";
+
+// Funtions and components
 import {
   formatPrice,
   PageColor,
 } from "../../../components/AnotherComponentsAndFunctions/AnotherComponentsAndFunctions";
 import ProductCart from "../../../components/ProductCart/ProductCart";
-import { BsTruck } from "react-icons/bs";
+import { BsTruck, BsCheckSquare } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const MemoizedProductCart = React.memo(ProductCart);
 
 const Cart = () => {
   const dispatch = useDispatch();
   const shippingData = useSelector((state) => state.shipping.data);
+  const loading = useSelector((state) => state.shipping.loading);
   const cart = useSelector((state) => state.cart);
 
   const [cep, setCep] = useState("");
-
-  console.log(shippingData);
+  const [selectedShipping, setSelectedShipping] = useState({
+    PrazoEntrega: null,
+    Valor: 0,
+    localidade: null,
+    tipo: "sedex",
+  });
 
   useEffect(() => {
     PageColor("white");
@@ -43,6 +54,8 @@ const Cart = () => {
       return totalPrice + productAndQuantity;
     }, 0);
   };
+
+  console.log(selectedShipping);
 
   return (
     <div>
@@ -82,54 +95,164 @@ const Cart = () => {
               </div>
               <button
                 onClick={handleSearchCep}
-                className="text-white bg-black hover:opacity-80 border w-12 h-12 mt-4 ml-2 rounded"
+                className="text-white bg-black hover:opacity-80 border p-2 h-12 mt-4 ml-2 rounded"
               >
-                Usar
+                Confirmar
               </button>
             </div>
-            {shippingData && shippingData.localidade && (
-              <div>
-                <div>
-                  <p className="font-bold text-lg mt-4">
-                    {shippingData.localidade}-{shippingData.uf}
-                  </p>
-                  <p>{shippingData.logradouro}</p>
-                  <p>{shippingData.bairro}</p>
-                </div>
-                {shippingData.prazoEPreco.map((item) => (
-                  <div
-                    className="flex items-center text-lg bg-slate-900 hover:opacity-80 mt-4 p-2 rounded cursor-pointer"
-                    key={item.Codigo}
-                  >
-                    <BsTruck size={20} className="text-white mr-2" />
-                    <p className="text-white font-bold mr-4">R$ {item.Valor}</p>
-                    <p className="text-white font-bold">
-                      Prazo: {item.PrazoEntrega} dia(s) úteis
-                    </p>
+            {loading ? (
+              <p>Carregando...</p>
+            ) : (
+              <>
+                {shippingData && shippingData.localidade && (
+                  <div>
+                    <div>
+                      <p className="font-bold text-lg mt-4">
+                        {shippingData.localidade}-{shippingData.uf}
+                      </p>
+                      <p>{shippingData.logradouro}</p>
+                      <p>{shippingData.bairro}</p>
+                    </div>
+
+                    <p className="mt-2">Selecione o tipo de envio</p>
+                    <label
+                      id="sedex"
+                      className={`flex items-center justify-between text-lg hover:opacity-80 mt-2 p-2 rounded cursor-pointer ${
+                        selectedShipping.tipo === "sedex"
+                          ? "bg-green-500"
+                          : "bg-black"
+                      }`}
+                    >
+                      <BsTruck size={20} className="text-white" />
+                      <p className={`text-white`}>
+                        Rápido: {shippingData.prazoEPreco?.[0]?.PrazoEntrega}{" "}
+                        dia(s) úteis
+                      </p>
+                      <p className={`text-white ml-4`}>
+                        {shippingData.localidade === "Governador Valadares" ? (
+                          <p
+                            className={`text-bold ${
+                              selectedShipping.tipo === "sedex"
+                                ? "text-black"
+                                : "text-green-500"
+                            }`}
+                          >
+                            Grátis
+                          </p>
+                        ) : (
+                          <p>R$ {shippingData.prazoEPreco?.[0]?.Valor}</p>
+                        )}
+                      </p>
+                      {selectedShipping === "sedex" ? "" : <BsCheckSquare />}
+                      <input
+                        type="radio"
+                        name="shippingOption"
+                        id="sedex"
+                        className="hidden"
+                        onClick={() =>
+                          setSelectedShipping({
+                            PrazoEntrega:
+                              shippingData.prazoEPreco?.[0]?.PrazoEntrega,
+                            Valor:
+                              shippingData.localidade === "Governador Valadares"
+                                ? 0
+                                : shippingData.prazoEPreco?.[0]?.Valor,
+                            localidade: shippingData.localidade,
+                            tipo: "sedex",
+                          })
+                        }
+                      />
+                    </label>
+
+                    <label
+                      id="pac"
+                      className={`flex items-center justify-between text-lg hover:opacity-80 mt-2 p-2 rounded cursor-pointer ${
+                        selectedShipping.tipo === "pac"
+                          ? "bg-green-500"
+                          : "bg-black"
+                      }`}
+                    >
+                      <BsTruck size={20} className="text-white" />
+                      <p className={`text-white`}>
+                        econômico: {shippingData.prazoEPreco?.[1]?.PrazoEntrega}{" "}
+                        dia(s) úteis
+                      </p>
+                      <p className={`text-white ml-4`}>
+                        {shippingData.localidade === "Governador Valadares" ? (
+                          <p
+                            className={`text-bold ${
+                              selectedShipping.tipo === "pac"
+                                ? "text-black"
+                                : "text-green-500"
+                            }`}
+                          >
+                            Grátis
+                          </p>
+                        ) : (
+                          <p>R$ {shippingData.prazoEPreco?.[1]?.Valor}</p>
+                        )}
+                      </p>
+                      {selectedShipping === "pac" ? "" : <BsCheckSquare />}
+                      <input
+                        type="radio"
+                        name="shippingOption"
+                        id="pac"
+                        className="hidden"
+                        onClick={() =>
+                          setSelectedShipping({
+                            PrazoEntrega:
+                              shippingData.prazoEPreco?.[1]?.PrazoEntrega,
+                            Valor:
+                              shippingData.localidade === "Governador Valadares"
+                                ? 0
+                                : shippingData.prazoEPreco?.[1]?.Valor,
+                            localidade: shippingData.localidade,
+                            tipo: "pac",
+                          })
+                        }
+                      />
+                    </label>
                   </div>
-                ))}
-              </div>
+                )}
+              </>
             )}
           </div>
           <div className="w-96">
             <div className="flex flex-col">
               <h2 className="font-bold text-2xl">Resumo do pedido</h2>
-              <div className="flex justify-between">
+              <div className="flex justify-between mt-2 border-b">
                 <p className="">Subtotal: </p>
                 <p>{formatPrice(calculateTotalPrice())}</p>
               </div>
-              <div className="flex justify-between">
+              <div className="flex justify-between mt-2 border-b">
                 <p>Frete:</p>
-                <p>Grátis</p>
+                {shippingData.localidade === "Governador Valadares" ? (
+                  <p className="text-bold text-green-500">Grátis</p>
+                ) : (
+                  <p>
+                    {formatPrice(selectedShipping && selectedShipping.Valor)}
+                  </p>
+                )}
               </div>
-              <div className="flex justify-between">
+              <div className="flex justify-between mt-2 border-b">
                 <p className="">Total do pedido: </p>
-                <p>{formatPrice(calculateTotalPrice())}</p>
+                <p>
+                  {formatPrice(
+                    calculateTotalPrice() +
+                      (selectedShipping && selectedShipping.Valor
+                        ? parseFloat(selectedShipping.Valor.replace(",", "."))
+                        : 0)
+                  )}
+                </p>
               </div>
             </div>
-            <button className="text-white bg-black w-44 h-12 p-2 m-2 rounded-md">
-              Continuar
-            </button>
+            <div className="flex justify-end">
+              <Link to="/revisar">
+                <button className="text-white bg-black w-44 h-12 p-2 m-2 mt-5 rounded-md">
+                  Continuar
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
